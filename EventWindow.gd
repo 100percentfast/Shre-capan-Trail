8c1240499f45594c9a25ae8f8406cbedd978df95
100percentfast <42898358+100percentfast@users.noreply.github.com>
2018-11-27 04:22:59 -0500

    Added EventArt functionality to Event Window.
    It's a little jank right now, but I'll fix that soon enough. I'm using stock art atm to test the system.
    
    To add an image to an event:
    
    1. move the image to the resource directory in Godot.
    2. copy the path of the image.
    3. paste the path into the EventArt component of the desired Event.
    4. ???
    5. PROFIT

--- EventWindow.gd
+++ EventWindow.gd
@@ -8,15 +8,24 @@ func _ready():
 
 
 func _on_Event_event_picked(event, choice, location):
+
 	$Choices.update_choices(choice, location)
-		# choice handler.
+		# choice handler. Updates the buttons to match loaded event.
 
 	popup()
 		# makes the popup do its thing. Sproing.
+
+	if event[1] == "none":
+		$EventArt.texture = load("res://GlobalAssets/EventArt/Placeholder.jpg")
+	else:
+		$EventArt.texture = load(event[1])
+		# Changes the EventArt to match the event's content.
+		# If no art is there, it changes it to Placeholder.jpg.
+
 	$EventText.text = event[0]
-		#changes the EventText to match the event's content.
+		# changes the EventText to match the event's content.
 
 
 func _on_Choices_event_picked(event, choice, location):
 	_on_Event_event_picked( event, choice, location )
-
+		# passthrough from player choice. Changes events.
